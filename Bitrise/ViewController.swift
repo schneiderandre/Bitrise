48951d2356e6a79ca03a59460006f8c85ad4b026
André Schneider <mail@schneiderandre.com>
2016-01-19 16:37:13 +0100

    Initial commit

---
+++ Bitrise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bitrise
+//
+//  Created by André Schneider on 19/01/16.
+//  Copyright © 2016 André Schneider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
