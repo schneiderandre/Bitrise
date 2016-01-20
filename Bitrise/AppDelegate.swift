15f0cfa0496b2b68f19ad760a4f13421543ef0bf
André Schneider <mail@schneiderandre.com>
2016-01-20 11:35:20 +0100

    Fix linter warnings

--- Bitrise/AppDelegate.swift
+++ Bitrise/AppDelegate.swift
@@ -13,13 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         if let controller = self.window?.rootViewController as? ViewController {
             let viewModel = ViewModel()
             controller.viewModel = viewModel
         }
         return true
     }
-
 }
-
